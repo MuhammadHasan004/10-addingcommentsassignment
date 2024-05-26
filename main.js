95900a87d93e0c1924f4416dab559d6e66d6aa81
MuhammadHasan004 <hasanmuhammadhm1122@gmail.com>
2024-05-26 14:09:51 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,4 @@
+// Muhammad hasan dated 26/april
+// adding comments now 
+var hisname = "Muhammad Hasan";
+console.log("".concat(hisname, ", would ypu like to go to USA"));
---
+++ main.ts
@@ -0,0 +1,4 @@
+// Muhammad hasan dated 26/april
+// adding comments now 
+let hisname:string="Muhammad Hasan";
+console.log(`${hisname}, would ypu like to go to USA`);
